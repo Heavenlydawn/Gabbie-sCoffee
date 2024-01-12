de3b2e26b471b7846779b8ac47b6b77a8bad34ce
Heaven <gabrielheavenlydawn@gmail.com>
2024-01-12 16:16:54 +0100

    Home Page Modified

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Haven',
+  title: "Gabbie's Coffee",
   description: 'A Taste Of Heaven',
 }
 
---
+++ src/components/CoffeeLang/CoffeeLang.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Image from 'next/image'
+import CoffeeBg from "../../../public/images/coffeee-bg.png"
+import Coffee1 from "../../../public/images/coffeee-1.png"
+import Coffee2 from "../../../public/images/coffeee-2.png"
+const CoffeeLang = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default CoffeeLang
