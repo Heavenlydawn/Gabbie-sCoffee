1b05b8665c26a72683429891844bc61553978eb6
Heaven <gabrielheavenlydawn@gmail.com>
2024-02-01 10:50:30 +0100

    Title Changed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "Riellah's Cafe ",
+  title: "Riellah's Cafe",
   description: 'A Taste Of Heaven',
 }
 
