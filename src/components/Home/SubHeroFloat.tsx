bb198e02deab1e16c3eee7e894f0444dc79df6a2
Heaven <gabrielheavenlydawn@gmail.com>
2024-02-01 10:35:11 +0100

    SubFloat Modification

--- src/components/Home/SubHeroFloat.tsx
+++ src/components/Home/SubHeroFloat.tsx
@@ -4,7 +4,7 @@ import CoffeeIcon from "../../../public/images/coffee-icon.png";
 const SubHeroFloat = () => {
   return (
     <div>
-      <div className="bg-[#ECDAC4] md:rounded-[80px] md:w-[878px] md:h-[188px] py-8 px-24 flex justify-around items-center md:flex-row flex-col h-[550px] gap-9">
+      <div className="bg-[#ECDAC4] md:rounded-[80px] md:w-[878px] md:h-[188px] py-8 px-24 flex justify-around items-center md:flex-row flex-col h-[750px] gap-9">
         <div className="flex justify-center items-center flex-col gap-4 w-[170px] h-12 text-center">
           <div className="bg-white bg-opacity-35 p-4 rounded-full">
             <Image src={CoffeeIcon} alt="Icon" />
